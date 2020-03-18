4c5ada6f36f1c99c1c7271d5d948ca69dba1fbff
Edina-n <62246968+Edina-n@users.noreply.github.com>
2020-03-18 16:01:21 +0000

    Asset Starter porjects
    assets unity folder

---
+++ Assets/Scripts/SkinScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SkinScript : MonoBehaviour
+{
+
+    public GameObject cube;
+    public GameObject sphere;
+    public GameObject caps;
+    public Button rightButton; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+
+        this.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        this.transform.position = new Vector3(0f, -3f, 0); 
+ 
+    }
+
+    public void OnMouseUp()
+    {
+        this.transform.localScale = new Vector3(1f, 1f, 1f);
+
+    }
+
+}
